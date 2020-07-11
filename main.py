7063089a75e7f043b54ce91a75d19261a3f68f6f
Jamil Bousquet <jamil.bousquet@gmail.com>
2020-07-11 18:12:17 -0400

    Init: added curses and window initialization; readme

---
+++ main.py
@@ -0,0 +1,54 @@
+from playsound import playsound
+# from tkinter import filedialog
+# from tkinter import *
+from pathlib import Path
+import curses
+from curses import wrapper
+
+# - ask for directory
+# - list songs in command window
+# - choose song
+# - give option for shuffle, repeat, etc
+
+# initialize cmd to run curse
+stdscr = curses.initscr()
+
+# turn off key echoing in cmd
+curses.noecho()
+# execute commands without 'Enter'
+curses.cbreak()
+# allow curses to handle special nav keys
+stdscr.keypad(True)
+
+# the following 3 statements terminates curses
+curses.nocbreak()
+stdscr.keypad(False)
+curses.echo()
+
+# restore cmd to default
+curses.endwin()
+
+# window dimensions
+begin_x = 20
+begin_y = 7
+height = 5
+width = 40
+
+# initialize window
+win = curses.newwin(height, width, begin_y, begin_x)
+
+
+def main(stdscr):
+    # Clear screen
+    stdscr.clear()
+
+    # This raises ZeroDivisionError when i == 10.
+    for i in range(0, 11):
+        v = i - 10
+        stdscr.addstr(i, 0, '10 divided by {} is {}'.format(v, 10 / v))
+
+    stdscr.refresh()
+    stdscr.getkey()
+
+
+wrapper(main)
