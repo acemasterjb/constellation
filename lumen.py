718ad11fc6a0de4afa45ce6eecd6ee60b0a0daa7
Jamil Bousquet <jamil.bousquet@gmail.com>
2020-07-30 21:15:34 -0400

    lumen.py: deprecated; learned how to do playback without threading

--- lumen.py
+++ lumen.py
@@ -1,31 +1,53 @@
 from libs.playsound.playsound import playsound
+from time import sleep
 from curses import KEY_ENTER
 
 class player:
 
     def __init__(self, *arugments, **keywords):
         self.__running = True
+        self.paused = False
+        # self.resume = False
 
     def stop(self):
         self.__running = False
 
-    def run(self, disp, song):
+    def pause(self, c):
+        self.paused = True
+        c.acquire()
+
+    def resume(self, c):
+        self.paused = False
+        c.notify()
+        c.release()
+
+    def run(self, disp, song, e, c):
         music_player = playsound()
         music_player.play(song, block=False)
-        song_position = None
+        e.set()
+        # song_position = None
 
         while self.__running:
-            key = disp.getch()
-            is_enter = (key == KEY_ENTER or key in [10, 13])
-
-            if key == ord('q') or is_enter:
-                music_player.stop()
-                self.stop()
-            if key == ord('p'):
-                if music_player.get_status() == 'paused':
-                    music_player.play(song, block=False, pos=song_position)
-                    song_position = None
-                else:
-                    song_position = music_player.get_position()
-                    music_player.stop()
+            sleep(0.1)
+            # key = disp.getch()
+            # is_enter = (key == KEY_ENTER or key in [10, 13])
+            # if key == ord('q') or is_enter:
+            #     music_player.stop()
+            #     self.stop()
+            with c:
+                while self.paused:
+                    music_player.pause()
+                    c.wait()
+                # if self.resume:
+                #     music_player.resume()
+                #     self.resume = False
+            # if key == ord('p'):
+            #     if music_player.get_status() == 'paused':
+            #         music_player.play(song, block=False, pos=song_position)
+            #         song_position = None
+            #         disp.clear()
+            #         disp.refresh()
+            #     else:
+            #         song_position = music_player.get_position()
+            #         music_player.stop()
                 # print(pos)
