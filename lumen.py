cadbd6acbc0888981bccbc590653dd68a8497ff4
Jamil Bousquet <jamil.bousquet@gmail.com>
2020-08-01 19:54:18 -0400

    Lumens now handles playback and keystrokes to control playback.

--- lumen.py
+++ lumen.py
@@ -1,53 +1,21 @@
 from libs.playsound.playsound import playsound
 from time import sleep
-from curses import KEY_ENTER
-
+from keyboard import is_pressed
 class player:
 
     def __init__(self, *arugments, **keywords):
         self.__running = True
-        self.paused = False
-        # self.resume = False
+        self.music_player = playsound()
 
-    def stop(self):
-        self.__running = False
-
-    def pause(self, c):
-        self.paused = True
-        c.acquire()
-
-    def resume(self, c):
-        self.paused = False
-        c.notify()
-        c.release()
-
-    def run(self, disp, song, e, c):
-        music_player = playsound()
-        music_player.play(song, block=False)
-        e.set()
-        # song_position = None
-
+    def run(self, song, block=False):
+        self.music_player.play(song, block)
         while self.__running:
             sleep(0.1)
-            # key = disp.getch()
-            # is_enter = (key == KEY_ENTER or key in [10, 13])
-            # if key == ord('q') or is_enter:
-            #     music_player.stop()
-            #     self.stop()
-            with c:
-                while self.paused:
-                    music_player.pause()
-                    c.wait()
-                # if self.resume:
-                #     music_player.resume()
-                #     self.resume = False
-            # if key == ord('p'):
-            #     if music_player.get_status() == 'paused':
-            #         music_player.play(song, block=False, pos=song_position)
-            #         song_position = None
-            #         disp.clear()
-            #         disp.refresh()
-            #     else:
-            #         song_position = music_player.get_position()
-            #         music_player.stop()
-                # print(pos)
+            if is_pressed('ctrl+q'):
+                self.music_player.stop()
+                self.__running = False
+            if is_pressed('p'):
+                if self.music_player.get_status() == 'playing':
+                    self.music_player.pause()
+                    continue
+                self.music_player.resume(False)
