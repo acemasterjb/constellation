9c838af2ee6c57179ed9cb723d355983de951903
Jamil Bousquet <jamil.bousquet@gmail.com>
2020-08-07 19:23:07 -0400

    Added support for FLAC container; currently can not show FLAC metadata in data panel; widgets/panels added for file browser and metadata viewer.

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from tkinter import *
 import curses
 from curses import wrapper
 from keyboard import is_pressed
+from time import sleep
+import eyed3
+import libs.soundfile as sf
 
 # - ask for directory
 # - list songs in command window
@@ -56,6 +59,36 @@ def getdir(seek=0, wdir=musiclib):
     return(root.items)
 
 
+def is_audio(file):
+    if file.endswith(('.mp3',)):
+        return True
+    return False
+
+
+def print_data(disp, menu, selected):
+
+    file = menu[selected].path
+
+    if menu[selected].is_file():
+        audio = eyed3.load(file)
+        disp.clear()
+        disp.box()
+
+        song_name = audio.tag.title
+        artist = audio.tag.artist
+        album = audio.tag.album
+
+        metadata = [song_name, artist, album]
+
+        for i in range(len(metadata) - 1):
+            disp.addstr(i, 1, metadata[i])
+            disp.addstr('\n')
+
+        disp.refresh()
+    else:
+        disp.clear()
+
+
 def print_items(disp, menu, selected):
     """
         Prompt curses to print each entry in a given menu.
@@ -71,13 +104,13 @@ def print_items(disp, menu, selected):
     for i, item in enumerate(menu):
         if i == selected:
             disp.attron(curses.color_pair(1))
-        disp.addstr(i, 0, item.name)
+        disp.addstr(i + 1, 1, item.name)
         disp.attroff(curses.color_pair(1))
 
     disp.refresh()
 
 
-def nav_menu(disp, menu, selected):
+def nav_menu(disp, meta, menu, selected):
     """
         Logic and actions for directory items.
 
@@ -99,21 +132,37 @@ def nav_menu(disp, menu, selected):
         if key == curses.KEY_UP and selected > 0:
             selected -= 1
             print_items(disp, menu, selected)
+            if is_audio(menu[selected].path):
+                print_data(meta, menu, selected)
         if key == curses.KEY_DOWN and selected < len(menu) - 1:
             selected += 1
             print_items(disp, menu, selected)
+            if is_audio(menu[selected].path):
+                print_data(meta, menu, selected)
 
         if is_enter:
             if menu[selected].is_dir():
                 top_dir = os.path.dirname(menu[0])
                 prev = getdir(seek=1, wdir=os.path.dirname(top_dir))
+
                 sel_path = menu[selected].path
                 menu = getdir(1, sel_path)
                 selected = 0
+
                 print_items(disp, menu, selected)
 
             elif menu[selected].is_file():
-                p.play(menu[selected].path, False)
+                playing = menu[selected].path
+                if menu[selected].path.endswith(('.flac')):
+                    with open(playing, 'rb') as f:
+                        data, file_stream = sf.read(f)
+                        sf.write("__play_temp.wav", data, file_stream)
+                    del(data)
+                    del(file_stream)
+                    p.play("__play_temp.wav", False)
+                    continue
+
+                p.play(playing, False)
 
         if key == ord('p'):
             if p.get_status() == 'playing':
@@ -126,11 +175,17 @@ def nav_menu(disp, menu, selected):
         if is_pressed('ctrl+q'):
             if p.get_status() in ['playing', 'paused']:
                 p.stop()
+                p.close_alias()
+                if os.path.exists("__play_temp.wav"):
+                    os.remove("__play_temp.wav")
             break
 
         if key == ord('q'):
             menu = prev
             print_items(disp, menu, 0)
+            top_dir = os.path.dirname(menu[0])
+            prev = getdir(seek=1, wdir=os.path.dirname(top_dir))
+        sleep(0.025)
 
 
 def main(stdscr):
@@ -139,14 +194,17 @@ def main(stdscr):
     curses.curs_set(False)
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
     y, x = stdscr.getmaxyx()
-    disp = curses.newwin(y - 3, x - 3, 3, 3)
-    disp.keypad(True)
+    disp = curses.newwin(y, x, 0, 0)
+    meta_panel = disp.subpad(int(y * 0.89), int(x * 0.19), 0, 0)
+    files = disp.subpad(int(y * 0.89), int(x * 0.82), 0, int(x * 0.19))
+    files.keypad(True)
+    # seek = disp.subpad(int(y * 0.14), x, int(y * 0.89), 0)
     # cmd = curses.newwin(5, x, 0, 0)
 
     curr_row = 0
     contents = getdir()
 
-    nav_menu(disp, contents, curr_row)
+    nav_menu(files, meta_panel, contents, curr_row)
 
     # disp.refresh()
     # disp.getkey()
