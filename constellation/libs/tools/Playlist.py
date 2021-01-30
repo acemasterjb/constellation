b09fbc45a76767047a5c528ff626c34927313732
Jamil Bousquet <jamil.bousquet@gmail.com>
2021-01-30 02:23:05 -0400

    Playlist class: added sort function for albums

--- constellation/libs/tools/Playlist.py
+++ constellation/libs/tools/Playlist.py
@@ -6,19 +6,25 @@ class Node:
 
 
 def swap(Set, i1, i2):
-    temp = Set[i1]
-    Set[i1] = Set[i2]
-    Set[i2] = temp
+    next_node = Set[i2].next[0]
+    temp_node = Set[i1]
+    prev_node = Set[i1 - 1]
+    del Set[i2].next
+    del prev_node.next
+    prev_node.next = (Set[i2],)
+    Set[i2].next = (temp_node,)
+    del temp_node.next
+    temp_node.next = (next_node,)
 
 
 def partition(Set, start, stop):
     mid = (start + stop) / 2
     swap(Set, start, mid)
     pivot_i = start
-    pivot_val = ord(Set[start])
+    pivot_val = Set[start].album
 
     for scan in range(start + 1, stop):
-        if ord(Set[scan]) < pivot_val:
+        if Set[scan].album < pivot_val:
             pivot_i += 1
             swap(Set, pivot_i, scan)
 
@@ -26,11 +32,11 @@ def partition(Set, start, stop):
     return pivot_i
 
 
-def sort_alpha(Set, start, stop):
+def sort_album(Set, start, stop):
     if start < stop:
         pivot = partition(Set, start, stop)
-        sort_alpha(Set, start, pivot - 1)
-        sort_alpha(Set, pivot + 1, stop)
+        sort_album(Set, start, pivot - 1)
+        sort_album(Set, pivot + 1, stop)
 
 
 class Playlist:
@@ -86,10 +92,93 @@ class Playlist:
             self.s += 1
 
     def sort(self, by):
-        pass
+        index = self.head
+
+        while index.next[0]:
+            pass
+
+    def __getitem__(self, index):
+        if isinstance(index, slice):
+            indices = range(*index.indices(self.s))
+            return [self[i].value for i in indices]
+        elif type(index) is not int:
+            raise ValueError
+            return
+        elif abs(index) > self.s:
+            raise IndexError
+            return
+
+        i = 0
+        node = [None]
+        node[0] = self.head
+
+        if index <= -1:
+            index = self.s + index
+
+        while i != index:
+            node[0] = node[0].next[0]
+            i += 1
+
+        try:
+            """to catch index == self.s"""
+            return node[0]
+        except AttributeError:
+            raise KeyError
+
+    def __setitem__(self, index, value):
+        node = [self.__getitem__(index)]
+        node[0].value = value
 
     def __iter__(self):
         return self
 
     def __next__(self):
+        if (self.s > 0) and (self.curr is None):
+            raise StopIteration
+        else:
+            if type(self.curr.value) is bytes:
+                self.curr.value = self.curr.value.decode('utf-8')
+            current = self.curr
+            self.curr = self.curr.next[0]
+            return current
 
+    def __delitem__(self, index):
+        if not self.head:
+            raise KeyError
+
+        node = [self.head]
+        prev = [None]
+        i = 0
+
+        if index < 0:
+            if abs(index) > self.s:
+                raise IndexError
+                return
+            else:
+                index = self.s + index
+
+        if index == 0:
+            node[0] = self.head.next[0]
+            del self.head
+            self.head = node[0]
+        else:
+            while node[0] and i != index:
+                prev[0] = node[0]
+                node[0] = node[0].next[0]
+                i += 1
+
+            if node[0]:
+                del prev[0].next
+                prev[0].next = (node[0].next[0],)
+                del node[0]
+        self.s -= 1
+
+    def __del__(self):
+        node = self.head
+        next_node = None
+
+        while node:
+            next_node = node.next[0]
+
+            del node
+            node = next_node
